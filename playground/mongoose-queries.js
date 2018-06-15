2f1e88f32a577dd3245abc69238a91e08d5e3cd0
Amandeep <amandeepsaini336@gmail.com>
2018-06-15 18:28:11 +0700

    Add GET/todo/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -10,24 +10,24 @@ if(!ObjectID.isValid(id)) {
   console.log('ID not found');
 }
 
-// Todo.find({
-//     _id: id
-// }).then((todos) => {
-//   console.log('Todos by find', todos);
-// });
-//
-// Todo.findOne({
-//   _id: id
-// }).then((todos) => {
-//   console.log('Todos by findOne', todos);
-// });
+Todo.find({
+    _id: id
+}).then((todos) => {
+  console.log('Todos by find', todos);
+});
 
-// Todo.findById(id).then((todos) => {
-//   if(!todos) {
-//     return console.log('Id not found');
-//   }
-//   console.log('Todos by findById', todos);
-// }).catch((e) =>console.log('Id invalid'));
+Todo.findOne({
+  _id: id
+}).then((todos) => {
+  console.log('Todos by findOne', todos);
+});
+
+Todo.findById(id).then((todos) => {
+  if(!todos) {
+    return console.log('Id not found');
+  }
+  console.log('Todos by findById', todos);
+}).catch((e) =>console.log(e));
 
 User.findById('5b22a99c61ea7e47b095d7c4').then((user) => {
   if(!user) {
