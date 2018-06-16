207e79ac2c0332705faabb6429cc45647a95539f
Amandeep <amandeepsaini336@gmail.com>
2018-06-16 13:24:39 +0700

    Heroku app deploy

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds259820.mlab.com:59820/amandeep_mongodb || mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
