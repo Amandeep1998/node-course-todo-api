b7a2f660dd5c61d0d48d59612d34731aabbe29bf
Amandeep <amandeepsaini336@gmail.com>
2018-06-16 22:56:55 +0700

    Some deploying tweak

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://amandeep:Rooneyrocks32@ds261540.mlab.com:61540/amandeep || mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
