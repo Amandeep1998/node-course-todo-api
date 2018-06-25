eedfef890db5dd8fd027cfdc1735b08526e3e429
Amandeep <amandeepsaini336@gmail.com>
2018-06-25 18:35:53 +0700

    Add GET/users/me and auntheticate method

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const _ =  require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+var {authenticate} = require('./middleware/authenticate');
 const port = process.env.PORT;
 
 var app = express();
@@ -96,8 +96,6 @@ app.post('/users', (req, res) => {
     email: body.email,
     password: body.password
   })
-
-
   user.save().then(() => {
     return user.generateAuthTokens();
     // res.send(val);
@@ -107,6 +105,11 @@ app.post('/users', (req, res) => {
     res.status(400).send(e);
   });
 
+})
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 })
 
 
